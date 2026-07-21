bfcb69e43ba2daa166a5d93dc4fa3535d83647b9
oliverfeher19 <oliver.feher19@gmail.com>
2026-07-20 22:56:44 -0400

    made updates to check email page

--- frontend/src/pages/CheckEmail.tsx
+++ frontend/src/pages/CheckEmail.tsx
@@ -2,24 +2,32 @@ import "./CheckEmail.css"
 import Logo from "../components/Logo";
 import Background from "../components/Background";
 
+import { Link } from "react-router-dom";
+
 function CheckEmail(){
     return(
-        <div className="signup-page">
+        <div className="email-check">
 
             <Logo />
 
             <Background />
 
-            <p>
+            <h3 className="message">
                 Your BudgetPet account has been created!
+            </h3>
 
-                We've sent a verification email to: example@example.com
+            <p className="sent-email">
+                We've sent a verification email to: example@example.com,
+                please click the link in that email to activate your account.
+            </p>
 
-                Please click the link in that email to activate your account.
-
+            <p className="no-email">
                 Didn't receive it? *Resend Verification Email*
+            </p>
 
-                Already verified? *Go to Login*
+            <p className="verified">
+                Already verified?
+                <Link to="/">Login</Link>
             </p>
 
         </div>
