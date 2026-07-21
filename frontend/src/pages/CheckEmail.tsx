23c3a223bd3192fa660e49cfe58d2d217c9d780f
oliverfeher19 <oliver.feher19@gmail.com>
2026-07-21 17:06:27 -0400

    updated check email page

--- frontend/src/pages/CheckEmail.tsx
+++ frontend/src/pages/CheckEmail.tsx
@@ -2,9 +2,16 @@ import "./CheckEmail.css"
 import Logo from "../components/Logo";
 import Background from "../components/Background";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function CheckEmail(){
+    const location = useLocation();
+    const email = location.state?.email ?? " the email address you provided";
+
+    function handleResendEmail(){
+
+    }
+
     return(
         <div className="email-check">
 
@@ -12,24 +19,40 @@ function CheckEmail(){
 
             <Background />
 
-            <h3 className="message">
-                Your BudgetPet account has been created!
-            </h3>
+            <div className="email-card">
 
-            <p className="sent-email">
-                We've sent a verification email to: example@example.com,
-                please click the link in that email to activate your account.
-            </p>
+                <h2 className="message">
+                    Verify Your Email
+                </h2>
 
-            <p className="no-email">
-                Didn't receive it? *Resend Verification Email*
-            </p>
+                <p className="sent-email">
+                    Your BudgetPet account has been created!
+                    <br /><br />
+                    We've sent a verification email to
+                    <br />
+                    <strong>{email}</strong>
+                    <br /><br />
+                    Please click the link in that email to activate your account.
+                </p>
 
-            <p className="verified">
-                Already verified?
-                <Link to="/">Login</Link>
-            </p>
+                <p className="resend-text">
+                    Didn't receive it?
+                </p>
 
+                <button
+                    className="resend-btn"
+                    onClick={handleResendEmail}
+                >
+                    Resend Verification Email
+                </button>
+
+                <p className="verified">
+                    Already verified?
+                    {" "}
+                    <Link to="/">Login</Link>
+                </p>
+
+            </div>
         </div>
     );
 }
