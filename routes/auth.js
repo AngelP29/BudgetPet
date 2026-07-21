4131e574560dfecfbe752142a3effcd17d29ed0c
oliverfeher19 <oliver.feher19@gmail.com>
2026-07-21 19:59:03 -0400

    updated register route

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require("crypto");
+
 const User = require('../models/User');
 const Pet = require('../models/Pet');
 
@@ -46,8 +48,22 @@ router.post('/register', async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
+        const verificationToken = crypto.randomBytes(32).toString("hex");
+        const verificationTokenExpires = new Date(
+            Date.now() + 24 * 60 * 60 * 1000
+        );
+
         // Save new user
-        const user = new User({ firstName, lastName, username, email, password: hashedPassword });
+        const user = new User({ 
+            firstName, 
+            lastName, 
+            username, 
+            email, 
+            password: hashedPassword,
+            isVerified: false,
+            verificationToken,
+            verificationTokenExpires
+        });
         await user.save();
 
         // AUTOMATICALLY CREATE STARTER PET FOR THIS USER
