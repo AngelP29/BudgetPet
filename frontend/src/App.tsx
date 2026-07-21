99162b0b6e9c25afe46a78144fcd315585dd8e8c
oliverfeher19 <oliver.feher19@gmail.com>
2026-07-20 22:08:43 -0400

    added new route to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SignUp from "./pages/SignupPage"
 import Dashboard from "./pages/Dashboard";
+import CheckEmail from "./pages/CheckEmail";
 
 function App() {
   return(
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/check-email" element={<CheckEmail />} />
       </Routes>
     </BrowserRouter>
   )
